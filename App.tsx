373f7f153bf9f4d9d393ff65fbf8e5540df6b1f8
Epyonn <pdavisnguyen1@outlook.com>
2024-02-14 12:46:57 -0800

    Add Gym Logos

--- App.tsx
+++ App.tsx
@@ -25,6 +25,9 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import Map from './components/Map'; 
+
+
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
@@ -63,36 +66,8 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <Map /> 
+
   );
 }
 
@@ -116,3 +91,4 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+4
---
+++ components/Map.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Image } from 'react-native';
+
+
+interface Gym {
+  id: string;
+  latitude: number;
+  longitude: number;
+  image: any;
+}
+import gw1 from '../assets/GWPC.png';
+import gw2 from '../assets/gwpower-co-icon.png';
+
+
+const gyms: Gym[] = [
+  { id: '3', latitude: 37.78825, longitude: -122.4324, image: gw1 },
+  { id: '2', latitude: 37.788825, longitude: -122.4324, image: gw2 },
+  // Add more gyms as needed
+];
+
+const Map: React.FC = () => {
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    >
+      {gyms.map((gym) => (
+        <Marker
+          key={gym.id}
+          coordinate={{ latitude: gym.latitude, longitude: gym.longitude }}
+          title={`Gym ${gym.id}`}
+        >
+            <Image source={gym.image} style={{ width: 800, height: 300 }} />
+        </Marker>
+      ))}
+    </MapView>
+  );
+};
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+});
+
+export default Map;
